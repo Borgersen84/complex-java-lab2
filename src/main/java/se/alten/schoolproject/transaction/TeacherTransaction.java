2530d3ce4349f246abc18e5ce1a03f52190387b5
Öyvind <oybo84@gmail.com>
2019-12-18 13:12:11 +0100

    method for adding teacher to database

--- src/main/java/se/alten/schoolproject/transaction/TeacherTransaction.java
+++ src/main/java/se/alten/schoolproject/transaction/TeacherTransaction.java
@@ -1,12 +1,36 @@
 package se.alten.schoolproject.transaction;
 
+import se.alten.schoolproject.entity.Teacher;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.NotFoundException;
 import java.util.List;
 
+@Stateless
+@Default
 public class TeacherTransaction implements TeacherTransactionAccess  {
 
+    @PersistenceContext(unitName="school")
+    private EntityManager entityManager;
 
     @Override
     public List listAllTeachers() {
         return null;
     }
+
+    @Override
+    public Teacher addTeacher(Teacher teacherToAdd) throws Exception {
+        try {
+            entityManager.persist(teacherToAdd);
+            entityManager.flush();
+            return teacherToAdd;
+        } catch (Exception e) {
+            throw new Exception("This didnt work");
+        }
+    }
+
+
 }
--- src/main/java/se/alten/schoolproject/transaction/TeacherTransactionAccess.java
+++ src/main/java/se/alten/schoolproject/transaction/TeacherTransactionAccess.java
@@ -1,8 +1,11 @@
 package se.alten.schoolproject.transaction;
 
+import se.alten.schoolproject.entity.Teacher;
+
 import java.util.List;
 
 public interface TeacherTransactionAccess {
 
     List listAllTeachers();
+    Teacher addTeacher(Teacher teacherToAdd) throws Exception;
 }
